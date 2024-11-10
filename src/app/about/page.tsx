e003120a2730cfce41a1ed305d8c090c86768f62
Kousar-Sheikh-Abbas <sheikhkousar1441@gmail.com>
2024-11-10 12:50:57 +0500

    first commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function about() {
+  return (
+    <div className="text-[60px] uppercase text-center font-bold mb-8">
+      <h1>About</h1>
+    </div>
+  )
+}
+
+export default about
---
+++ src/app/destination/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function destination() {
+  return (
+    <div className="text-[60px] uppercase text-center font-bold mb-8">
+    <h1>Destination</h1>
+  </div>
+  )
+}
+
+export default destination
